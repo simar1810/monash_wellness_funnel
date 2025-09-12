9a08f76ab52a7450437d52c902c7f72fe5ee485d
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2025-09-12 18:43:54 +0530

    fix: basic changes landing page

--- src/components/SocialProof.jsx
+++ src/components/SocialProof.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 
 const whoShouldAttend = [
@@ -26,28 +27,37 @@ const whoShouldAttend = [
 
 export default function WhoShouldAttendSection() {
   return (
-    <section className="w-full py-12 md:py-16 bg-white">
+    <section className="w-full py-12 md:py-16 bg-[#eeece0]">
       <div className="max-w-[1200px] mx-auto px-4 md:px-6">
         <h2 className="text-2xl md:text-4xl font-bold text-center text-secondary mb-8">
           Stay Sharp & Energetic At Work—Every Day
         </h2>
 
-        <div className="mt-[30px] md:mt-[60px] flex flex-wrap text-black items-center justify-center gap-[28px]">
-          {whoShouldAttend.map((item) => (
-            <div
-              key={item.id}
-              className="relative w-full md:w-[calc(50%-32px)] h-[200px] md:h-[130px] rounded-[10px] overflow-hidden bg-red-600"
-            >
-              <div className="absolute inset-0 bg-[var(--accent)] flex items-center gap-4 px-6 py-4">
-                <span className="text-white text-[22px] md:text-[30px] font-bold bg-secondary px-4 py-2 rounded-full">
-                  {item.id.toString().padStart(2, "0")}
-                </span>
-                <p className="text-white text-[18px] md:text-[22px] font-bold leading-tight">
-                  {item.description}
-                </p>
+        <div className="w-full mt-[30px] md:mt-[60px] lg:flex text-black items-center justify-center gap-[28px]">
+          <div className="md:min-w-[50%]">
+            {whoShouldAttend.map((item) => (
+              <div
+                key={item.id}
+                className="relative w-full mb-4 h-[80px] md:h-[90px] rounded-[10px] overflow-hidden bg-red-600"
+              >
+                <div className="absolute inset-0 bg-[var(--accent)] flex items-center gap-4 px-6 py-4">
+                  <span className="text-white text-[22px] md:text-[30px] font-bold bg-secondary px-4 py-2 rounded-full">
+                    {item.id.toString().padStart(2, "0")}
+                  </span>
+                  <p className="text-white text-[18px] md:text-[22px] font-bold leading-tight">
+                    {item.description}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <Image
+            src="/11_days.png"
+            height={1024}
+            width={1024}
+            alt=""
+            className="w-full mt-8 max-h-[300px] md:max-h-[350px] object-contain"
+          />
         </div>
       </div>
     </section>
